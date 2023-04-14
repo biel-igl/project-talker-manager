3ae190905d0a7d7fc9be3d28a11f288e80ad7ca6
biel-igl <gabrielpires524@gmail.com>
2023-04-14 17:21:33 -0300

    feat: faz o get com um id retornando a pessoa com o id correto

--- src/index.js
+++ src/index.js
@@ -17,6 +17,14 @@ app.get('/talker', async (_req, res) => {
   return res.status(200).json(talker); 
 });
 
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talker = await readJson('talker.json');
+  const talkeById = talker.find((one) => one.id === Number(id));
+  if (!talkeById) res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  return res.status(200).json(talkeById); 
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
